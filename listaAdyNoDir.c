f43b3e135c8972181e1d64a5d7d948bd503e1300
Victoria Sánchez <victoria.sanchez@utp.edu.co>
2025-03-26 12:12:45 -0500

    Update

--- listaAdyNoDir.c
+++ listaAdyNoDir.c
@@ -11,7 +11,7 @@ typedef struct Nodo
 
 typedef struct GrafoLista
 {
-    int lista[MAX_NODOS];
+    Nodo *lista[MAX_NODOS];
     int numNodos;
 } GrafoLista;
 
@@ -27,18 +27,18 @@ void inicializarLista(GrafoLista *g, int nodos)
 void agregarAristaLista(GrafoLista *g, int origen, int destino, int peso)
 {
     // Conexión en una dirección.
-    Nodo *nuevo1 = (Nodo *)malloc(sizeof(Nodo));
+    Nodo* nuevo1 = (Nodo*)malloc(sizeof(Nodo));
     nuevo1->destino = destino;
     nuevo1->peso = peso;
-    nuevo1->siguiente = g-- > lista[i];
+    nuevo1->siguiente = g->lista[origen];
     g->lista[origen] = nuevo1;
 
     // Conexión en dirección contraria para que sea bidireccional.
-    Nodo *nuevo2 = (Nodo *)malloc(sizeof(Nodo *));
+    Nodo* nuevo2 = (Nodo*)malloc(sizeof(Nodo));
     nuevo2->destino = origen;
     nuevo2->peso = peso;
     nuevo2->siguiente = g->lista[destino];
-    g->lista[origen] = nuevo2;
+    g->lista[destino] = nuevo2;
 }
 
 void imprimirListaCompleta(GrafoLista *g)
@@ -48,11 +48,11 @@ void imprimirListaCompleta(GrafoLista *g)
         if (g->lista[i] != NULL)
         {
             Nodo *temp = g->lista[i];
-            for (int j = temp; j != NULL; j = j->siguiente)
+            for (Nodo *j = temp; j != NULL; j = j->siguiente)
             { // Explora cada conexión.
                 if (i < j->destino)
                 {
-                    printf("The weight between the noodes %d and %d, is %d\n", i, j->destino, j->peso)
+                    printf("The weight between the noodes %d and %d, is %d\n", i, j->destino, j->peso);
                 }
             }
         }
@@ -63,7 +63,7 @@ int main()
 {
     int nodos = 3;
     GrafoLista *lista = (GrafoLista *)malloc(sizeof(GrafoLista));
-
+    inicializarLista(lista, MAX_NODOS); // Asegurar que el grafo empiece en un estado limpio y válido.
     // Se establecen los valores para vértices, aristas y peso.
     agregarAristaLista(lista, 1, 5, 4);
     agregarAristaLista(lista, 2, 0, 3);
